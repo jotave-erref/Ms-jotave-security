f2d8669948ef698fe99ea0a152dab6b726abc7c5
jotave-erref <jvrferreira96@gmail.com>
2025-11-04 18:10:29 -0300

    feat(services): Register ms-condominios and ms-sensores with Eureka

--- ms-condominios/src/main/java/br/com/jotavesecurity/ms_condominios/MsCondominiosApplication.java
+++ ms-condominios/src/main/java/br/com/jotavesecurity/ms_condominios/MsCondominiosApplication.java
@@ -2,8 +2,10 @@ package br.com.jotavesecurity.ms_condominios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsCondominiosApplication {
 
 	public static void main(String[] args) {
