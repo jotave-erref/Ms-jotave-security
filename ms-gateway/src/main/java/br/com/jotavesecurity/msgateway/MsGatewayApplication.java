88609390b6a051512ea00d88e810e43e0375fc00
jotave-erref <jvrferreira96@gmail.com>
2025-12-03 11:48:54 -0300

    refactor(gateway): migrate config to yaml, update routes and containerize

--- ms-gateway/src/main/java/br/com/jotavesecurity/msgateway/MsGatewayApplication.java
+++ ms-gateway/src/main/java/br/com/jotavesecurity/msgateway/MsGatewayApplication.java
@@ -1,11 +1,9 @@
-package br.com.jotavesecurity.ms_gateway;
+package br.com.jotavesecurity.msgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MsGatewayApplication {
 
 	public static void main(String[] args) {
--- ms-gateway/src/test/java/br/com/jotavesecurity/msgateway/MsGatewayApplicationTests.java
+++ ms-gateway/src/test/java/br/com/jotavesecurity/msgateway/MsGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.jotavesecurity.ms_gateway;
+package br.com.jotavesecurity.msgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
